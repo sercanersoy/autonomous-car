b0021d5b9a504bcdaa7d5a6369cfb7bf598902ba
Sercan Ersoy <sercanersoy5@gmail.com>
2020-07-07 12:02:30 +0300

    delete commented out parts

--- main.c
+++ main.c
@@ -79,33 +79,6 @@ void handle_push_button_pressed(void) {
 
 #endif
 
-// joystick pins collide with sonic sensor pins, so dont use it
-/*
-void handle_joystick_pressed(void) {
-	switch (joystick_pressed()) {
-		case JOYSTICK_UP:
-			action_enum_to_action(ACTION_MOVE_FORWARD);
-			led_turn_on_solo(0);
-			break;
-		
-		case JOYSTICK_RIGHT:
-			action_enum_to_action(ACTION_SLIGHT_RIGHT);
-			led_turn_on_solo(1);
-			break;
-		
-		case JOYSTICK_DOWN:
-			action_enum_to_action(ACTION_SHARP_RIGHT);
-			led_turn_on_solo(2);
-			break;
-		
-		case JOYSTICK_LEFT:
-			action_enum_to_action(ACTION_SLIGHT_LEFT);
-			led_turn_on_solo(3);
-			break;
-	}
-}
-*/
-
 void update(void) {
 	for (int i = 0; i < n_sensors; i++) {
 		if ((ultrasonic_updated & (1 << i)) != 0) {
@@ -122,49 +95,10 @@ void update(void) {
 	if (push_button_pressed) {
 		handle_push_button_pressed();
 	}
-	
-	/*
-	if (joystick_pressed()) {
-		handle_joystick_pressed();
-	}
-	*/
 }
 
 int main(void) {
 	init();
-	
-	/*
-	static int16_t test_input[INPUT_SIZE * VEC_DIM] = {INPUT_DATA};
-	static uint8_t correct_output[INPUT_SIZE] = {CORRECT_LABEL};
-	
-	int16_t * pInput = test_input;
-	uint8_t * pCorrect = correct_output;
-	
-	uint16_t n_true = 0;
-	uint16_t true_count[VEC_DIM];
-	uint16_t pred_count[VEC_DIM];
-	uint16_t count[VEC_DIM];
-	
-	memset(true_count, 0, VEC_DIM * sizeof(uint16_t));
-	memset(pred_count, 0, VEC_DIM * sizeof(uint16_t));
-	memset(count, 0, VEC_DIM * sizeof(uint16_t));
-	
-	for (int i = 0; i < INPUT_SIZE; i++) {
-		mlp_predict(pInput);
-		pInput += VEC_DIM;
-		
-		if (mlp_result == *pCorrect) {
-			n_true++;
-			true_count[result]++;
-		}
-		
-		pred_count[result]++;
-		count[*pCorrect++]++;
-	}
-	
-	// breakpoint here to see the result in debug mode
-	float32_t accuracy = (float32_t) n_true / INPUT_SIZE;
-	*/
 	
 	while(1) {
 		update();
